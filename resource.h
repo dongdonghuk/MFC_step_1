df6f63103d92e50dcdc6a2a4f9fdca488e206f79
dongdonghuk <jay5713665@gmail.com>
2022-06-21 18:28:19 +0900

    리소스 수정

--- resource.h
+++ resource.h
@@ -13,13 +13,33 @@
 #define IDM_SPLIT_2                     32775
 #define IDM_SPLIT_4                     32776
 #define IDM_IMAGE_LOAD                  32777
+#define ID_32778                        32778
+#define ID_32779                        32779
+#define ID_32780                        32780
+#define ID_32781                        32781
+#define ID_32782                        32782
+#define ID_32783                        32783
+#define ID_32784                        32784
+#define ID_32785                        32785
+#define ID_32786                        32786
+#define ID_32787                        32787
+#define ID_32788                        32788
+#define ID_32789                        32789
+#define ID_32790                        32790
+#define ID_32791                        32791
+#define ID_32792                        32792
+#define IDM_IMG_FLIP                    32793
+#define IDM_IMG_FLIP_RL                 32794
+#define IDM_IMG_FLIP_UD                 32795
+#define ID_FLIP_LF                      32796
+#define ID_FLIP_UD                      32797
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        310
-#define _APS_NEXT_COMMAND_VALUE         32778
+#define _APS_NEXT_COMMAND_VALUE         32798
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
